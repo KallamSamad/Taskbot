067c6dd2745a624edb2f6a2ba1036a897ff4ab89
Kallam <kallamsamad@gmail.com>
2026-02-07 01:57:25 +0000

    Added an assets folder with images folder in it. Polished CSS a bit and used flexbox to split the ui into thirds.

--- index.php
+++ index.php
@@ -3,9 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>TaskBot</title>
 </head>
 <body>
+    <div class="header">
+        
     
+        <div class="left">
+                <img class="waterfall" alt="Image of a waterfall">
+        </div>
+        
+        <div class="middle">
+            <div class="trilayer">
+                <div class="layer1">
+                <h1>Welcome To Taskbot</h1>
+                </div>
+                <div class="layer2">
+                world
+                </div>
+                <div class="layer3">
+                hello
+                </div>
+            </div>
+
+        </div>
+        
+        
+        <div class="right">Devilsdwdaasdasjho</div>
+        
+        </div>
+<div class="footer"></div>
 </body>
 </html>
