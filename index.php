3eddcc3238a6db3078f887787175c94e9d1e140c
Kallam <kallamsamad@gmail.com>
2026-02-07 06:13:02 +0000

    created a logo with a robot image added to a note image in paint, did the css for the middle section of the index page added a good colour scheme to ensure its eye catching such as font style, size, etc

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>TaskBot</title>
 </head>
 <body>
@@ -15,15 +16,32 @@
         </div>
         
         <div class="middle">
-            <div class="trilayer">
+            <div class="quadlayer">
                 <div class="layer1">
-                <h1>Welcome To Taskbot</h1>
+                    <h1 style="text-align:center">Taskbot</h1>
+                    <img class="logo" src="Assets/Images/logo.png" alt="logo of a green robot with a check marked">
                 </div>
                 <div class="layer2">
-                world
+                <h2 style="padding-top:30px;">What is Taskbot?</h2>
+                <p class="paragraph">Taskbot is a very intuitive and structured way of organising tasks. Be it day-to-day "to do lists" to planning a project for your work. If you want an effiecnt way of organising your tasks, Taskbot is the app!</p>
                 </div>
                 <div class="layer3">
-                hello
+
+                <div class="dead"> 
+                <img src="Assets/Images/deadline.jpg" alt="An image of a calendar" height="50px">    
+                <h2>Deadlines and Status</h2>
+                </div>
+                <p class="paragraph">You can change the <b>status</b> of a task from <b>pending</b> to <b>completed</b>. You can also set deadlines to see if the task is done.</p>
+                 
+                <div class="group">
+                <img src="Assets/Images/team.png" alt="An image of a team working" height="50px">    
+                <h2>Grouping Tasks</h2>
+                </div>
+                 <p class="paragraph">You can <b>group</b> similar tasks into a bigger task list to share with users who you are working with. <i>Great</i> for <b>teamwork</b>.</p>
+
+                </div>
+                <div class="layer4">
+
                 </div>
             </div>
 
@@ -33,6 +51,6 @@
         <div class="right">Devilsdwdaasdasjho</div>
         
         </div>
-<div class="footer"></div>
+<div class="footer">By Kallam Samad 2026 </div>
 </body>
 </html>
